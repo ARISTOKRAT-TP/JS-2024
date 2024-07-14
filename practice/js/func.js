be5e6d18208249eee650aa85094f0a94190a6a38
Ekaterina Pokatova <150853636+EkaterinaPokatova@users.noreply.github.com>
2024-07-15 02:36:41 +0300

    1
    -

---
+++ practice/js/func.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // конечная дата = 9 сентября 2024 (переписать потом, что дата будет считываться из окошка, в которое пользователь ее введет)
+    const deadline = new Date(2024, 9, 9);
+
+    // id таймера
+    let timerId = null;
+
+    // склонение числительных
+    function declensionNum(n, text_forms) {  
+        n = Math.abs(n) % 100; 
+        let n1 = n % 10;
+        if (n > 10 && n < 20) { return text_forms[2]; }
+        if (n1 > 1 && n1 < 5) { return text_forms[1]; }
+        if (n1 == 1) { return text_forms[0]; }
+        return text_forms[2];
+    }
+
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+      //изменяем подписи
+      $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+
+    // вызываем функцию countdownTimer
+    countdownTimer();
+
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
