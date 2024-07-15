8753bd7dec2edfd8393b7ba033a56e44e088c9d6
Dmitri Belov <dbelov1357@gmail.com>
2024-07-15 11:50:27 +0300

    main page, catalog page

---
+++ project/app.js
@@ -0,0 +1,28 @@
+const categoriesFileName = 'public/assets/categories.json';
+
+const fs = require('fs');
+const express = require("express");
+const app = express();
+
+app.set("view engine", "hbs");
+app.use(express.static('public'));
+
+
+app.use("/catalog", function(_, response) {
+  var categoriesArray = makeCategories();
+
+  response.render("catalog.hbs", {
+      categories: categoriesArray
+  });
+});
+
+app.use("/", function(_, response) {
+  response.render("index.hbs");
+});
+
+app.listen(3000);
+ 
+function makeCategories()
+{
+  return JSON.parse(fs.readFileSync(categoriesFileName).toString()).categories;
+}
