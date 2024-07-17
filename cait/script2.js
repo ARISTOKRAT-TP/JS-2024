f8f39484491560c8dee210635317b2e4c7c318f2
Artyom <61425754+ARISTOKRAT-TP@users.noreply.github.com>
2024-07-17 19:09:15 +0300

    project

---
+++ cait/script2.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("back-to-top");
+ 
+  // Показать/скрыть кнопку при прокрутке страницы
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+});
