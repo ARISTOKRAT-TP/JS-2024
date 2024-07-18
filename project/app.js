cfa6ef146c606e38314cf50d6b6cea0f8f24967a
Dmitri Belov <dbelov1357@gmail.com>
2024-07-18 19:38:02 +0300

    uni template for good page + upd app

--- project/app.js
+++ project/app.js
@@ -12,51 +12,142 @@ app.set("view engine", "hbs");
 app.use(express.static('public'));
 
 app.use("/:goodType/:goodName", function(request, response) {
-  var goodsArray;
+  var selectedGood;
+  var goodDescription;
+
   switch(request.params.goodType) {
     case 'processors':
-      goodsArray = JSON.parse(fs.readFileSync(processorsFileName).toString()).goods;
+      var goodsArray = JSON.parse(fs.readFileSync(processorsFileName).toString()).goods;
+
+      goodsArray.forEach(element => 
+        {
+          if(element.link == ('/' + request.params.goodType + '/' + request.params.goodName))
+          {
+            selectedGood = element;
+            goodDescription = {
+              "Бренд": selectedGood.brand,
+              "Серия": selectedGood.seria,
+              "Сокет": selectedGood.socket,
+              "Ядра": selectedGood.cores,
+              "Тип памяти": selectedGood.ddr,
+              "Частота": selectedGood.freq,
+              "Технологический процесс": selectedGood.tech_proc
+            };
+          return;
+          }
+        }
+      );
+
+      break;
+
+    case 'graphics-cards':
+      var goodsArray = JSON.parse(fs.readFileSync(graphicsCardsFileName).toString()).goods;
+      
+      goodsArray.forEach(element => 
+        {
+          if(element.link == ('/' + request.params.goodType + '/' + request.params.goodName))
+          {
+            selectedGood = element;
+            goodDescription = {
+              "Бренд": selectedGood.brand,
+              "Графический процессор": selectedGood.g_processor,
+              "Объем видеопамяти": selectedGood.v_memory,
+              "Тип памяти": selectedGood.ddr,
+              "Максимальное энергопотребление": selectedGood.max_energy,
+              "Техпроцесс": selectedGood.tech_proc
+            };
+          return;
+          }
+        }
+      );
+
+      break;
+
+    case 'motherboards':
+      var goodsArray = JSON.parse(fs.readFileSync(motherboardsFileName).toString()).goods;
+      
+      goodsArray.forEach(element => 
+        {
+          if(element.link == ('/' + request.params.goodType + '/' + request.params.goodName))
+          {
+            selectedGood = element;
+            goodDescription = {
+              "Бренд": selectedGood.brand,
+              "Сокет": selectedGood.socket,
+              "Тип памяти": selectedGood.ddr,
+              "Слоты для памяти": selectedGood.slots,
+              "Чипсет": selectedGood.chipset
+            };
+          return;
+          }
+        }
+      );
+
+      break;
+
+    case 'ram':
+      var goodsArray = JSON.parse(fs.readFileSync(ramFileName).toString()).goods;
+      
+      goodsArray.forEach(element => 
+        {
+          if(element.link == ('/' + request.params.goodType + '/' + request.params.goodName))
+          {
+            selectedGood = element;
+            goodDescription = {
+              "Бренд": selectedGood.brand,
+              "Объем памяти": selectedGood.value,
+              "Тип памяти": selectedGood.ddr,
+              "Частота": selectedGood.freq,
+              "Латентность": selectedGood.latency,
+              "Напряжение": selectedGood.voltage
+            };
+          return;
+          }
+        }
+      );
+
+      break;
+
+  }
+
+  response.render('good.hbs', {
+      good: selectedGood,
+      goodDescr: goodDescription
+    }
+  );
+
+});
+
+app.use("/catalog", function(_, response) {
+  var categoriesArray = JSON.parse(fs.readFileSync(categoriesFileName).toString()).categories;;
+
+  response.render("catalog.hbs", {
+      categories: categoriesArray
+  });
+});
+
+app.use("/:category", function(request, response) {
+  var categoryFileName;
+  
+  switch(request.params.category) {
+    case 'processors':
+      categoryFileName = processorsFileName;
       break;
     case 'graphics-cards':
-      goodsArray = JSON.parse(fs.readFileSync(graphicsCardsFileName).toString()).goods;
+      categoryFileName = graphicsCardsFileName;
       break;
     case 'motherboards':
-      goodsArray = JSON.parse(fs.readFileSync(motherboardsFileName).toString()).goods;
+      categoryFileName = motherboardsFileName;
       break;
     case 'ram':
-      goodsArray = JSON.parse(fs.readFileSync(ramFileName).toString()).goods;
+      categoryFileName = ramFileName;
       break;
   }
-  
-  goodsArray.forEach(element => {
-    if(element.link == ('/' + request.params.goodType + '/' + request.params.goodName))
-    {
-      response.render(request.params.goodType + '.hbs', {
-        good: element
-      });
-    }
-  });
 
-});
-
-app.use("/processors", function(_, response) {
-  renderGoodsPage(processorsFileName, response);
-});
-
-app.use("/graphics-cards", function(_, response) {
-  renderGoodsPage(graphicsCardsFileName, response);
-});
-
-app.use("/motherboards", function(_, response) {
-  renderGoodsPage(motherboardsFileName, response);
-});
-
-app.use("/ram", function(_, response) {
-  renderGoodsPage(ramFileName, response);
+  renderGoodsPage(categoryFileName, response);
 });
 
 function renderGoodsPage(goodsFileName, response) {
-
   var goodsArray = JSON.parse(fs.readFileSync(goodsFileName).toString()).goods;
 
   response.render("goods.hbs", {
@@ -64,21 +155,8 @@ function renderGoodsPage(goodsFileName, response) {
   });
 }
 
-app.use("/catalog", function(_, response) {
-  var categoriesArray = makeCategories();
-
-  response.render("catalog.hbs", {
-      categories: categoriesArray
-  });
-});
-
 app.use("/", function(_, response) {
   response.render("index.hbs");
 });
 
 app.listen(3000);
-
-function makeCategories()
-{
-  return JSON.parse(fs.readFileSync(categoriesFileName).toString()).categories;
-}
