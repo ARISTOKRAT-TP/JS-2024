1fcc4c6ed0c64095e18e8247d739a74bf60d19d5
alena krylova <150517805+alenakrylova@users.noreply.github.com>
2024-07-18 21:23:02 +0300

    18.07

--- guess the number/js/script.js
+++ guess the number/js/script.js
@@ -1,5 +1,9 @@
 /**
  * btn010, btn0100, btn01000 - кнопки выбора диапазона загаданного числа.
+ * your_range - блок с формой, где пользователь вводит свой диапазон
+ * num1 - число, с которого начинается диапазон пользователя
+ * num2 - число, на которое диапазон пользователя заканчивается
+ * your_num - кнопка вводя диапазона пользователя
  * form - блок с формой ввода числа
  * result - блок с ответами. угадал ли пользователь
  * info -  строка с объяснениями игры
@@ -13,6 +17,10 @@
 let btn_010 = document.querySelector(".range10");
 let btn_0100 = document.querySelector(".range100");
 let btn_01000 = document.querySelector(".range1000");
+let your_range = document.querySelector(".your_range");
+let num1  = document.querySelector(".num1");
+let num2  = document.querySelector(".num2");
+let your_num = document.querySelector(".your_num");
 
 let form = document.querySelector(".form");
 let result = document.querySelector(".result");
@@ -39,7 +47,9 @@ let RandomNum=-1;
 let RandomNum10 = 1 + Math.floor(Math.random()*10);
 let RandomNum100 = 1 + Math.floor(Math.random()*100);
 let RandomNum1000 = 1 + Math.floor(Math.random()*1000);
+let min, max;
 let UserNum;
+
 
 
 /**
@@ -57,6 +67,7 @@ btn_010.onclick = function() {
     btn_010.style.display = "none";
     btn_0100.style.display = "none";
     btn_01000.style.display = "none";
+    your_range.style.display = "none";
 };
 
 btn_0100.onclick = function() {
@@ -67,6 +78,7 @@ btn_0100.onclick = function() {
     btn_010.style.display = "none";
     btn_0100.style.display = "none";
     btn_01000.style.display = "none";
+    your_range.style.display = "none";
 };
 
 btn_01000.onclick = function() {
@@ -77,6 +89,22 @@ btn_01000.onclick = function() {
     btn_010.style.display = "none";
     btn_0100.style.display = "none";
     btn_01000.style.display = "none";
+    your_range.style.display = "none";
+};
+
+your_num.onclick = function() {
+    min = Number(num1.value);
+    max = Number(num2.value);
+    let RandomNumUser =(Math.floor(Math.random()*(max - min +1)) + min);
+    RandomNum = RandomNumUser;
+    form.style.display = "block";
+    result.style.display = "block";
+    info.textContent = "Мы загадали случайное число от " + min + " до " + max +" . Попытайтесь его угадать.";
+    btn_010.style.display = "none";
+    btn_0100.style.display = "none";
+    btn_01000.style.display = "none";
+    your_range.style.display = "none";
+    console.log(min, max, RandomNumUser);
 };
 
 /**
