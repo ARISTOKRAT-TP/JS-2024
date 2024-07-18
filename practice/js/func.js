177292b7a1852641074f9f40d302ee4870fb830a
Ekaterina Pokatova <150853636+EkaterinaPokatova@users.noreply.github.com>
2024-07-18 19:54:10 +0300

    1
    1

--- practice/js/func.js
+++ practice/js/func.js
@@ -1,12 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
 
-    // конечная дата = 9 сентября 2024 (переписать потом, что дата будет считываться из окошка, в которое пользователь ее введет)
-    const deadline = new Date(2024, 9, 9);
+let deadline;
 
-    // id таймера
-    let timerId = null;
+//функция сохранения даты
+function save() {
+  console.log({day:document.getElementById('day').value})
+  let deadline1 = {day:document.getElementById('day').value};
+  deadline = new Date(deadline1.day);
+  console.log(deadline)
+  if (deadline != 'Invalid Date'){
+    alert('Дата сохранена');
+  } else {
+    alert('Введите дату!')
+  }
+}
 
-    // склонение числительных
+//функция отсчета времени
+function counting() {
+
+  if (!deadline || deadline == 'undefined'){
+    alert('Введите дату!')
+  }
+
+  diff0 = deadline - new Date();
+  if (diff0 <= 0) {
+    alert('Эта дата уже наступила. Выберите другую')
+  }
+
+
+    // склонение дней, часов, минут и секунд
     function declensionNum(n, text_forms) {  
         n = Math.abs(n) % 100; 
         let n1 = n % 10;
@@ -18,37 +39,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
     function countdownTimer() {
+
+    // id таймера
+    let timerId = null;
+
       const diff = deadline - new Date();
+
       if (diff <= 0) {
         clearInterval(timerId);
       }
+
       const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
-      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 - 3 : 0;
       const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
       const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
 
-
+      //устанавливаем количество дней, часов, минут, секунд
       $days.textContent = days < 10 ? '0' + days : days;
       $hours.textContent = hours < 10 ? '0' + hours : hours;
       $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
       $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
 
-      //изменяем подписи
+      // изменяем подписи под таймером
       $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
       $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
       $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
       $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
     }
 
-    // получаем элементы, содержащие компоненты даты
+    // получаем элементы, содержащие компоненты даты с сайта
     const $days = document.querySelector('.timer__days');
     const $hours = document.querySelector('.timer__hours');
     const $minutes = document.querySelector('.timer__minutes');
     const $seconds = document.querySelector('.timer__seconds');
-
+    
     // вызываем функцию countdownTimer
     countdownTimer();
-
+    
     // вызываем функцию countdownTimer каждую секунду
     timerId = setInterval(countdownTimer, 1000);
-  });
+
+  };
